cbc82de1b717daa6ca30c9be881dd661a6c80615
Galina Pudovkina <galinapudovkina8@mail.ru>
2023-09-22 12:04:12 +0300

    19.2

---
+++ catalog/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from catalog.models import Product, Category, Contact
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "price", "category")
+    list_display_links = ("pk", "name")
+    list_filter = ("category",)
+    list_fields = ("name", "description")
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("pk", "country", "inn", "address")
